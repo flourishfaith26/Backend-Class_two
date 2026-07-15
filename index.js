2b0399f48f057cdc7fc223615d877b982b8e924c
Odediran Blossom <flourishfaith26@gmail.com>
2026-07-15 11:01:02 -0700

    chore: initialize package.json for classtwo project with express dependency

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express()
+const port = 4549;
+const worldCup = [
+    {
+        Country: "Nigeria",
+        Player: "Victor Oshinem",
+        Coach: "Finidi George"
+    },
+    
+    {
+        Country: "England",
+        Player: "Harry Kane",
+        Coach: "Gareth Southgate"
+    },
+
+    {
+        Country: "Brazil",
+        Player: "Neymal Jr",
+        Coach: "Finidi George"
+    },
+    {
+        Country: "Germany",
+        Player: "Toni Kroos",
+        Coach: "Julian Spalter"
+    },
+
+    {
+        Country: "Italy",
+        Player: "Nicolo Barella",
+        Coach: "Luciano Spalletti"
+    },
+
+    {
+        Country: "France",
+        Player: "Kylian Mbappe",
+        Coach: "Finidi George"
+    }
+
+]
+
+
+app.get('/home', (request, response) => {
+    response.send('Welcome to Backend Class')
+    console.log('Welcome to my Backend Class');
+    
+
+    
+});
+
+app.get('/worldcup', (request, response) => {
+    response.send(worldCup)
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+
+})
